c4901c377d805527061d0320b4eaf8cff3463b61
Anand Apte <Anand Apte>
2017-06-12 19:15:31 +1000

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,22 @@ userSchema.methods.generateAuthToken = function () {
   });
 };
 
+userSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var Users = mongoose.model('User', userSchema);
 
 module.exports = {
