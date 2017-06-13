fd521a5cfecc0ea49072f426da59bb07e2f66215
Anand Apte <Anand Apte>
2017-06-13 17:12:47 +1000

    Added hashing to the passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,20 @@
 const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
 
-var message = 'Main tera hero!';
-var hash = SHA256(message).toString();
+var password = '123password';
 
-console.log(`Message: ${message}`);
-console.log(`Hashed Message: ${hash}`);
+bcrypt.genSalt(10,(err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  })
+})
+
+var hashedPassword = '$2a$10$Ahu6KM1MUdx9sHKVFcpiaeRuZ29m/X7mYxucLF8jlPj3a7oTc3CH6';
+bcrypt.compare('123password', hashedPassword, (err, res) => {
+  console.log(res);
+})
+// var message = 'Main tera hero!';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hashed Message: ${hash}`);
