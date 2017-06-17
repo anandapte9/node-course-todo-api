972ae0a6ee9612dfa18abbeb7393baad5a0c4d57
Anand Apte <Anand Apte>
2017-06-17 22:02:11 +1000

    added /user/login route

--- server/server.js
+++ server/server.js
@@ -104,6 +104,17 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+   Users.findByCredential(body.email, body.password).then((user) => {
+     return user.generateAuthToken().then((token) => {
+       res.header('x-auth', token).send(user);
+     });
+   }).catch((e) => {
+     res.status(400).send();
+   })
+});
+
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
